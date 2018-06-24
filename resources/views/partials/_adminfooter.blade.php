c1e7a06e954481b541b8c4f651aa13fc06bb6c55
Ink <nyarkorep@gmail.com>
2018-06-24 02:26:22 +0000

    The admin dashboard template

---
+++ resources/views/partials/_adminfooter.blade.php
@@ -0,0 +1,8 @@
+<!-- footer content -->
+<footer class="navbar navbar-inverse">
+  <div class="text-center">
+    <a href="{{ route('AdminIndex') }}"><b>Efiewuragh</b> 2018</a>
+  </div>
+  <div class="clearfix"></div>
+</footer>
+<!-- /footer content -->
---
+++ resources/views/partials/_adminjavascript.blade.php
@@ -0,0 +1,57 @@
+<!-- jQuery -->
+<!--  <script src="../vendors/jquery/dist/jquery.min.js"></script> -->
+
+<script src="{{ asset('js/jquery.js')}}"></script> 
+<!-- Bootstrap -->
+<!--  <script src="../vendors/bootstrap/dist/js/bootstrap.min.js"></script> -->
+
+<script src="{{ asset('js/bootstrap.min.js')}}"></script>
+<!-- FastClick -->
+<script src="{{ asset('js/fastclick.js')}}"></script>
+<!-- NProgress -->
+<script src="{{ asset('js/nprogress.js')}}"></script>
+<!-- Chart.js -->
+<script src="{{ asset('js/Chart.min.js')}}"></script>
+<!-- gauge.js -->
+<script src="{{ asset('js/gauge.min.js')}}"></script>
+<!-- bootstrap-progressbar -->
+<script src="{{ asset('js/bootstrap-progressbar.min.js')}}"></script>
+
+
+<!-- iCheck -->
+<script src="{{ asset('js/icheck.min.js')}}"></script>
+<!-- Skycons -->
+<script src="{{ asset('js/skycons.js')}}"></script>
+<!-- Flot -->
+<script src="{{ asset('js/jquery.flot.js')}}"></script>
+<script src="{{ asset('js/jquery.flot.pie.js')}}"></script>
+<script src="{{ asset('js/jquery.flot.time.js')}}"></script>
+<script src="{{ asset('js/jquery.flot.stack.js')}}"></script>
+<script src="{{ asset('js/jquery.flot.resize.js')}}"></script>
+<!-- Flot plugins -->
+<script src="{{ asset('js/jquery.flot.orderBars.js')}}"></script>
+<script src="{{ asset('js/jquery.flot.spline.min.js')}}"></script>
+<script src="{{ asset('js/curvedLines.js')}}"></script>
+<!-- DateJS -->
+<script src="{{ asset('js/date.js')}}"></script>
+<!-- JQVMap -->
+<script src="{{ asset('js/jquery.vmap.js')}}"></script>
+<script src="{{ asset('js/jquery.vmap.world.js')}}"></script>
+<script src="{{ asset('js/jquery.vmap.sampledata.js')}}"></script>
+<!-- bootstrap-daterangepicker -->
+<script src="{{ asset('js/moment.min.js')}}"></script>
+<script src="{{ asset('js/daterangepicker.js')}}"></script>
+
+<!-- Custom Theme Scripts -->
+<script src="{{ asset('js/custom.min.js')}}"></script>
+<!-- Google Analytics -->
+<script>
+(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+})(window,document,'script','../../../www.google-analytics.com/analytics.js','ga');
+
+ga('create', 'UA-23581568-13', 'auto');
+ga('send', 'pageview');
+
+</script>
